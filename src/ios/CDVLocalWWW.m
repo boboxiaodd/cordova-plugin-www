14fb9285f17fef62da1e7a82cb700319749a59a7
boboxiaodd <iam386@gmail.com>
2022-06-30 16:45:01 +0800

    add

--- src/ios/CDVLocalWWW.m
+++ src/ios/CDVLocalWWW.m
@@ -1,10 +1,23 @@
 #import <Cordova/CDV.h>
 #import "CDVLocalWWW.h"
+#import "SSZipArchive.h"
 
 @implementation CDVLocalWWW
-	- (void)pluginInitialize
-	{
-
-	}
+- (void)pluginInitialize
+{
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *srcPath = [[[NSBundle mainBundle] URLForResource:@"www" withExtension:@"zip"] absoluteString];
+    NSString *zipPath = [[NSURL URLWithString:srcPath] path];
+    NSArray *directoryPaths = [fileManager URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask];
+    NSURL *distPath = [[directoryPaths firstObject] URLByAppendingPathComponent:@"/"];
+    NSString *destinationPath = [distPath path];
+    NSError *error;
+    if([SSZipArchive unzipFileAtPath:zipPath toDestination:destinationPath overwrite:YES password:nil error:&error delegate:nil]) {
+        NSLog(@"unzip success!");
+    } else {
+        NSLog(@"%@ - %@", @"Error occurred during unzipping", [error localizedDescription]);
+    }
+    NSLog(@"re location: %@",[distPath absoluteString]);
+}
 
 @end
