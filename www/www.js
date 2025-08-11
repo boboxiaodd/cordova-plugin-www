013ecb30535bcddaf85fe8d0ae9b6c0c089ba4e6
boboxiaodd <iam386@gmail.com>
2025-08-11 10:56:03 +0800

    Update www.js

--- www/www.js
+++ www/www.js
@@ -27,6 +27,9 @@ const CDVLocalWWW = {
     },
     playBeep: function (){
         exec(null, null, 'CDVLocalWWW','playBeep',[]);
+    },
+    enableScreenShot: function(success,options){
+        exec(success, null, 'CDVLocalWWW','enableScreenShot',[options]);
     }
 };
 channel.onCordovaReady.subscribe(function () {
